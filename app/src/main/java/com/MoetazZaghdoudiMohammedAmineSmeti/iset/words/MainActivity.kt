df8ca383806060adfdf3daa8a4a9d9a9bdee13a0
Moetaz Zaghdoudi <wiztaz5@gmail.com>
2021-11-22 17:27:33 +0100

    Fragments and the Navigation Component

--- app/src/main/java/com/MoetazZaghdoudiMohammedAmineSmeti/iset/words/MainActivity.kt
+++ app/src/main/java/com/MoetazZaghdoudiMohammedAmineSmeti/iset/words/MainActivity.kt
@@ -3,6 +3,10 @@ package com.MoetazZaghdoudiMohammedAmineSmeti.iset.words
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.MoetazZaghdoudiMohammedAmineSmeti.words.R
 import com.MoetazZaghdoudiMohammedAmineSmeti.words.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +17,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
